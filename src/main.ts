eb3c7ccc5d7ee542334ba4c7066b5e65aab5e2f0
Юрий Кравцов <urijkravcov@MacBook-Air-von-Urij.local>
2025-06-27 16:39:18 +0500

    allow all cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://tandem-client-one.vercel.app', 'http://localhost:3000'],
+    origin: [
+      'https://tandem-client-one.vercel.app',
+      'http://localhost:3000',
+      '*',
+    ],
     credentials: true,
   });
-  await app.listen(process.env.PORT || 5555);
+  await app.listen(5555);
 }
 bootstrap();
